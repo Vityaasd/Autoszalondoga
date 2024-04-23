3ba8cdda344184eaa859b7e970daab8afcc3a3c6
Vityaasd <144451649+Vityaasd@users.noreply.github.com>
2024-04-23 12:58:13 +0200

    Add files via upload

---
+++ autoszalon/autoszalon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoszalon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Autosalon autosalon = new Autosalon();
+
+            autosalon.UjJarmuHozzaadasa(new Szemelyauto("Toyota", "Corolla", 2021, 8000000, 15000, 5, 6.5, "Fehér"));
+            autosalon.UjJarmuHozzaadasa(new Terepjaro("Land Rover", "Defender", 2020, 15000000, 2000, "4WD", true, 3500));
+            autosalon.JarmuvekListazasa();
+            autosalon.JarmuEladasa("Toyota", "Corolla");
+            autosalon.JarmuvekListazasa();
+        }
+    }
+}
---
+++ autoszalon/autoszalon/Szemelyauto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoszalon
+{
+    public class Szemelyauto : Jarmu
+    {
+        public int UlesekSzama { get; set; }
+        public double Fogyasztas { get; set; }
+        public string Szin { get; set; }
+
+        public Szemelyauto(string gyarto, string modell, int evjarat, int alapAr, int kmAllas, int ulesekSzama, double fogyasztas, string szin)
+            : base (gyarto, modell, evjarat, alapAr, kmAllas)
+        {
+            UlesekSzama = ulesekSzama;
+            Fogyasztas = fogyasztas;
+            Szin = szin;
+        }
+
+        public void KenyelmiExtraBeszerzese(string extra)
+        {
+            if (extra == "prémium hangrendszer")
+            {
+                AlapAr += 500000;
+            }
+            else if (extra == "napfénytető")
+            {
+                AlapAr += 300000;
+            }
+        }
+
+        public void SzinFrissitese(string ujSzin)
+        {
+            Szin = ujSzin;
+        }
+    }
+}
